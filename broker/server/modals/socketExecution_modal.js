8616c54f0399ed80f0790f260394eedc5c43562e
ConnieLiu <ring213@gmail.com>
2022-05-06 16:49:01 +0800

    add broker folder to the repo

--- broker/server/modals/socketExecution_modal.js
+++ broker/server/modals/socketExecution_modal.js
@@ -1,11 +1,15 @@
 let { mysqldb } = require('../../util/mysql')
 
 const updateOrderInfo = async (updateResult) => {
-  let [result] = mysqldb.query(
+  let [result] = await mysqldb.query(
     `SELECT remaining_quantity, execution_quantity FROM orderInfo WHERE orderID = ${updateResult.orderID}`
   )
-  let remaining_quantity = result[0].remaining_quantity - updateResult.quantity
-  let execution_quantity = result[0].execution_quantity + updateResult.execution_quantity
+
+  let remaining_quantity = result[0].remaining_quantity - updateResult.executionQuantity
+  if (remaining_quantity < 0) {
+    remaining_quantity = 0
+  }
+  let execution_quantity = result[0].execution_quantity + updateResult.executionQuantity
   let columns = {
     status: updateResult.orderStatus, //1: 委託成功, 2: 部分成交, 3: 完全成交
     remaining_quantity: remaining_quantity,
@@ -13,19 +17,18 @@ const updateOrderInfo = async (updateResult) => {
   }
   let sqlSyntax = `UPDATE orderInfo SET ? WHERE orderID = ?`
   await mysqldb.query(sqlSyntax, [columns, updateResult.orderID])
-  return
 }
 
 const createOrderHistory = async (updateResult) => {
   let columns = {
     orderID: updateResult.orderID,
     status: 3, //1: 委託成功, 3: 已成交 4: 減量成功
-    quantity: updateResult.execution_quantity,
-    order_price: updateResult.price,
+    quantity: updateResult.executionQuantity,
+    price: updateResult.price,
+    create_time: updateResult.orderTime,
   }
   let sqlSyntax = `INSERT INTO order_history SET ? `
   await mysqldb.query(sqlSyntax, columns)
-  return
 }
 
 module.exports = { updateOrderInfo, createOrderHistory }
--- broker/socketToExchange.js
+++ broker/socketToExchange.js
@@ -1,28 +1,52 @@
-const { io } = require('socket.io-client')
-const socketToClient = require('./util/socketToClient')
-const { updateOrderInfo, createOrderHistory } = require('./server/modals/socketExecution_modal')
+const { io } = require("socket.io-client");
+const socketToClient = require("./util/socketToClient");
+const {
+  updateOrderInfo,
+  createOrderHistory,
+} = require("./server/modals/socketExecution_modal");
 
-let socketToExchange = io('http://127.0.0.1:8080') //TODO:nginx要改
+let socketToExchange = io("http://127.0.0.1:8080"); //TODO:nginx要改
 
-const BROKERID = 6666
-socketToExchange.on('connect', () => {
-  socketToExchange.emit('brokerID', BROKERID)
-  console.log(`I am ${BROKERID} after emit`)
-})
+const BROKERID = 1030;
+socketToExchange.on("connect", () => {
+  socketToExchange.emit("brokerID", BROKERID);
+  console.log(`I am ${BROKERID} after emit`);
+  let test = 0;
+  socketToExchange.on("fiveTicks", function (message) {
+    console.log("[8000 socketToExchange] fiveTicks: ", message);
+    socketToClient.sendFiveTicks(message);
+  });
 
-socketToExchange.on('fiveTicks', function (message) {
-  // console.log('[8000 socketToExchange] fiveTicks: ', message)
-  socketToClient.sendFiveTicks(message)
-})
+  socketToExchange.on("execution", async function (message) {
+    console.log("[8000 socketToExchange] execution: ", message);
+    try {
+      await updateOrderInfo(message);
+      await createOrderHistory(message);
+    } catch (error) {
+      console.error(error);
+    }
+    socketToClient.sendExec(message.dealer, message);
+  });
 
-socketToExchange.on('execution', async function (message) {
-  console.log('[8000 socketToExchange] execution: ', message)
-  await updateOrderInfo(message)
-  await createOrderHistory(message)
-  socketToClient.sendExec(message.dealer, message)
-})
+  socketToExchange.on("kLine", function (message) {
+    // console.log('[8000 socketToExchange] kLine: ', message)
+    socketToClient.sendKLine(message);
+  });
+});
 
-socketToExchange.on('kLine', function (message) {
-  // console.log('[8000 socketToExchange] kLine: ', message)
-  socketToClient.sendKLine(message)
-})
+// socketToExchange.on("fiveTicks", function (message) {
+//   console.log("[8000 socketToExchange] fiveTicks: ", message);
+//   socketToClient.sendFiveTicks(message);
+// });
+
+// socketToExchange.on("execution", async function (message) {
+//   console.log("[8000 socketToExchange] execution: ", message);
+//   await updateOrderInfo(message);
+//   await createOrderHistory(message);
+//   socketToClient.sendExec(message.dealer, message);
+// });
+
+// socketToExchange.on("kLine", function (message) {
+//   // console.log('[8000 socketToExchange] kLine: ', message)
+//   socketToClient.sendKLine(message);
+// });
