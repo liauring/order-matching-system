c866c8c22f065fdd9cc2c3131e6e14882cc9441b
ConnieLiu <ring213@gmail.com>
2022-05-11 13:46:30 +0800

    count average of the time period

--- backend/workers/stressTest.js
+++ backend/workers/stressTest.js
@@ -2,24 +2,38 @@ require('dotenv').config({ path: __dirname + '/./../.env' })
 let { rabbitmqConn } = require('../util/rabbitmq')
 
 ;(async () => {
+  let totalPubRabbitmqToSub = 0
+  let totalrabbitmqToMatch = 0
+  let count = 0
   try {
     rabbitmqConn = await rabbitmqConn
     rabbitmqConn.consume(
       'matchTime',
       async (matchTime) => {
-        console.log(JSON.parse(matchTime.content))
+        // console.log(JSON.parse(matchTime.content))
         let matchTimeData = JSON.parse(matchTime.content)
         let pubRabbitmqToSub = matchTimeData[2] - matchTimeData[1]
         let rabbitmqToMatch = matchTimeData[3] - matchTimeData[2]
-        // let matchToExec = matchTimeData[4] - matchTimeData[3]
-        console.log(
-          'pubRabbitmqToSub: ',
-          pubRabbitmqToSub,
-          ' / rabbitmqToMatch: ',
-          rabbitmqToMatch
-          // ' / matchToExec: ',
-          // matchToExec
-        )
+
+        if (count === 0) {
+          count += 1
+          return
+        }
+
+        totalPubRabbitmqToSub += pubRabbitmqToSub
+        totalrabbitmqToMatch += rabbitmqToMatch
+        count += 1
+
+        console.log((totalPubRabbitmqToSub / count).toFixed(2), (totalrabbitmqToMatch / count).toFixed(2), count)
+        // console.log(
+        //   'pubRabbitmqToSub: ',
+        //   pubRabbitmqToSub,
+        //   ' / rabbitmqToMatch: ',
+        //   rabbitmqToMatch
+        //   // ' / matchToExec: ',
+        //   // matchToExec
+        // )
+
         rabbitmqConn.ack(matchTime)
       },
       { noAck: false }
