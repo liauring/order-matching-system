fa916cb97a1a6bcd7e53b18a3dea4baa7217c61a
ConnieLiu <ring213@gmail.com>
2022-05-23 19:52:57 +0800

    add clearRedis

---
+++ exchange/script/clearRedis.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: __dirname + '/./../.env' })
+require('../util/rabbitmq').rabbitmqCreateConnect()
+const redisClient = require('../util/redis')(async () => {
+  redisClient.flushdb(function (err, succeeded) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('[Clear Redis] redis flushall succeeded')
+    }
+  })
+  redisClient.disconnect()
+  process.exit(0)
+})
