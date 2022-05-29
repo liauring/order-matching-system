87dbfecd7a4cba4f06aa17e53f17e52b4e6754fa
ConnieLiu <ring213@gmail.com>
2022-05-29 16:24:46 +0800

    fix bugs

--- broker/server/controllers/newOrder_controller.js
+++ broker/server/controllers/newOrder_controller.js
@@ -3,7 +3,9 @@ const { createNewOrder, createNewOrderHistory } = require('../models/newOrder_mo
 
 const postNewOrder = async (req, res, next) => {
   let reqBody = req.body
+  console.log('broker getNewBody request', reqBody)
   let getNewBody = await axios.post(`${process.env.apiHost}/api/newOrder/orderID`, reqBody)
+  console.log('broker getNewBody response from exchange api', getNewBody)
   await createNewOrder(getNewBody.data)
   await createNewOrderHistory(getNewBody.data)
   let response = await axios.post(`${process.env.apiHost}/api/newOrder`, getNewBody.data)
